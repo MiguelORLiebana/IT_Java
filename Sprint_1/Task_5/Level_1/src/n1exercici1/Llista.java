8de564f981c248fc97f7e7f6e9eed1d4dc15d755
Miguel <miguelliebana83@gmail.com>
2024-03-09 17:53:51 +0100

    Splitting exercici1 in two classes

--- Sprint_1/Task_5/Level_1/src/n1exercici1/Llista.java
+++ Sprint_1/Task_5/Level_1/src/n1exercici1/Llista.java
@@ -4,14 +4,6 @@ import java.io.File;
 import java.util.*;
 
 public class Llista {
-    public static void main(String[] args) {
-
-//        String directory = "/Users/mike/Desktop";
-//        System.out.println(args[0]);
-//        System.out.println(args[0].getClass());
-
-        llistarContingut(args[0]);
-    }
 
     public static void llistarContingut(String directory) {
 
